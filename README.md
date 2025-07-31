5ba88974ab40d66f63c5b25670ba3ec98189c729
Mateo-Falco <mati.falco2@gmail.com>
2025-07-31 15:55:47 -0300

    update all files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
- a 
+#  Amigo Secreto
+
+Este es un pequeño proyecto web que permite gestionar una lista de amigos y sortear aleatoriamente uno de ellos. Ideal para jugar al "Amigo Invisible" o "Amigo Secreto".
+
+##  Funcionalidades
+
+* Agregar nombres a una lista de amigos.
+* Visualizar la lista en tiempo real.
+* Sortear aleatoriamente un amigo de la lista.
+* Mostrar el resultado en pantalla.
+
+## Vista previa
+![captura de pantalla del proyecto](assets/vista-previa.png)
+
+## Tecnologías usadas
+
+* HTML5
+* CSS3
+* JavaScript (Vanilla JS)
+
+## Estructura del proyecto
+index.html # Página principal
+style.css # Estilos personalizados
+app.js # Lógica JavaScript
+assets/ # Imágenes y otros recursos
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function mostrarAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function agregarAmigo(){
+    let amigo = document.querySelector("#amigo").value;
+    if (amigo === ""){
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigo);
+        console.log(amigos);
+        document.querySelector("#amigo").value = "";
+        mostrarAmigos();
+    }
+}
+
+function sortearAmigo(){
+    let resultado = document .getElementById('resultado');
+    
+    if (amigos.length === 0 ){
+        resultado.innerHTML = "No hay amigos para sortear";
+    }else{
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceAleatorio];
+        resultado.innerHTML = (`El amigo sorteado fue ${amigoSorteado}!`)
+    }
+    
+}
